3979cc1f9555c253c6b25bd72bfcb80e7ec558db
Rodrigo Peteno <rodrigopeteno@gmail.com>
2017-06-12 14:47:42 -0300

    Commit Inicial

---
+++ OO/src/main/java/br/com/munif/oo/Personagem.java
@@ -0,0 +1,45 @@
+package br.com.munif.oo;
+
+public abstract class Personagem implements Descritivel {
+
+    protected String nome;
+
+    private Lugar lugar;
+
+    public abstract void chora();
+
+    public Personagem(String nome, Lugar lugar) {
+        this.nome = nome;
+        this.lugar = lugar;
+        lugar.getPersonagens().add(this);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Lugar getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(Lugar lugar) {
+        if (this.lugar != null) {
+            this.lugar.getPersonagens().remove(this);
+        }
+        this.lugar = lugar;
+        this.lugar.getPersonagens().add(this);
+    }
+
+    @Override
+    public String descreve() {
+        return "Personagem com nome " + nome + " em " + lugar.getDescricao();
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+    
+    
+
+}
