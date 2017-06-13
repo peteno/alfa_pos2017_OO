8f155c4ea2963a35ac2e6517ea609662a7c9a4d5
Rodrigo Peteno <rodrigopeteno@gmail.com>
2017-06-12 22:48:57 -0300

    Commit Final

---
+++ OO/src/main/java/br/com/peteno/oo/main/Personagem.java
@@ -0,0 +1,16 @@
+package br.com.peteno.oo.main;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Personagem {
+
+	private String nome;
+
+	public void teste() {
+		String[] valores = {"sdfdf","sdfdf","sdfdf","sdfdf","sdfdf","sdfdf"};
+
+	}
+
+}
